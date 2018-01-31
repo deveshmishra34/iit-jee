f3ff1e2873a07c15a1c0a6d21552708d239c67f9
Devesh mishra <deveshmishra34@gmail.com>
2018-01-31 14:40:07 +0530

    add server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express"),
+    path = require("path"),
+    app = express();
+
+
+app.use(express.static(__dirname + "/dist"));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8085);
